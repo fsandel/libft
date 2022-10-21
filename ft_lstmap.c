b30f99dd498faa5942924a3f7be8855849e10bf4
Florian Sandel <fsandel@1-B-6.42heilbronn.de>
2022-10-21 21:48:29 +0200

    fixed lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: fsandel <fsandel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 16:05:46 by fsandel           #+#    #+#             */
-/*   Updated: 2022/10/21 21:18:03 by fsandel          ###   ########.fr       */
+/*   Updated: 2022/10/21 21:46:46 by fsandel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new;
 	t_list	*out;
 	t_list	*tail;
-	t_list	*temp;
 
 	if (!lst || !f || !del)
 		return (NULL);
@@ -25,15 +24,18 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	new = ft_lstnew(f(lst->content));
 	ft_lstadd_back(&out, new);
 	tail = out;
-	temp = lst;
 	lst = lst ->next;
 	while (lst)
 	{
 		new = ft_lstnew(f(lst->content));
+		if (new == NULL)
+		{
+			ft_lstclear(&out, del);
+			return (NULL);
+		}
 		ft_lstadd_back(&tail, new);
 		lst = lst ->next;
 		tail = tail->next;
 	}
-	//ft_lstclear(&temp, del);
 	return (out);
 }
