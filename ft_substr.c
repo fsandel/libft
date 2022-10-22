b977ea189e0c3f6a33a8def81ef2dd0578cfbab5
Florian Sandel <fsandel@1-A-25.42heilbronn.de>
2022-10-22 11:26:38 +0200

    better mem allocation

--- ft_substr.c
+++ ft_substr.c
@@ -6,11 +6,14 @@
 /*   By: fsandel <fsandel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 11:53:06 by fsandel           #+#    #+#             */
-/*   Updated: 2022/10/22 11:12:47 by fsandel          ###   ########.fr       */
+/*   Updated: 2022/10/22 11:25:07 by fsandel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+//calculates the minimum space needed for ft_substr and returns it
+static int	ft_calc_mem(char const *s, unsigned int start, size_t len);
 
 //copies string s, starts at atart, up to len chars
 char	*ft_substr(char const *s, unsigned int start, size_t len)
@@ -23,7 +26,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	length = ft_strlen(s);
 	if (start > length || len == 0)
 		return (ft_calloc(1, 1));
-	sub = ft_calloc(length - start + 2, 1);
+	sub = ft_calloc(ft_calc_mem(s, start, len) + 1, 1);
 	while (i < len && s[i])
 	{
 		sub[i] = s[i + start];
@@ -31,4 +34,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	}
 	sub[i] = '\0';
 	return (sub);
+}
+
+static int	ft_calc_mem(char const *s, unsigned int start, size_t len)
+{
+	size_t	s_length;
+
+	s_length = ft_strlen(s);
+	if ((s_length - start + 2) > len)
+		return (len);
+	else
+		return (s_length - start + 1);
 }
